4d9ab2570cd050161f14bd07d7a3f981584e018f
Igor <avt191540@yandex.ru>
2022-07-08 21:45:42 +0300

    solving the task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<User> users = new ArrayList<>();
+
+        System.out.println(findOldUser(users));
+    }
+
+    public static User findOldUser(List<User> users) {
+        Set<User> userSet = new HashSet<>(users);
+        List<User> userSort = new ArrayList<>(userSet);
+        Comparator<User> comp = Comparator.comparing(User::getAge).thenComparing(User::getName);
+        userSort.sort(comp);
+        return  userSort.get(userSort.size() - 1);
     }
 }
